26575a38836a26cf1d0087b2c02e6b147a52560a
lahcen001 <lahcen0000lh@gmail.com>
2020-07-14 20:21:20 +0200

    lahcen

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Controllers;
 use App\Order;
+use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class OrderController extends Controller
 {
 
     public function index() {
-        $orders = Order::orderBy('id', 'asc')->get();
-        return $orders->toJson();
+       
+    
+        $users = DB::table('users')
+        ->join('orders', 'users.id', '=', 'orders.user_id')
+        ->select('users.name', 'users.email', 'orders.price', 'orders.imageURL')
+       
+        ->get();
 
+        return response()->json($users, 201);
     }
+
+    
 
     public function orders(Request $request)
     {
